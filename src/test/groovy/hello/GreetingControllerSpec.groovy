6e859fe397f95a5522c78b8f4ba8bafdc3f5a0c3
Wei Jin <wei.jin@gmail.com>
2014-03-19 13:07:46 -0400

    rename file

--- src/test/groovy/hello/GreetingControllerSpec.groovy
+++ src/test/groovy/hello/GreetingControllerSpec.groovy
@@ -14,7 +14,7 @@ import java.util.concurrent.Executors
 import java.util.concurrent.Future
 import java.util.concurrent.TimeUnit
 
-public class GeetingControllerSpec extends Specification {
+public class GreetingControllerSpec extends Specification {
 
     @Shared
     @AutoCleanup
